d08e89a99aa11dfcfb25f9523ba1ef48721dcfdb
Jan Ballhorn <janhannes@outlook.de>
2024-08-07 22:24:00 +0200

    Repository Interface Return Types  überarbeitet

--- src/Repository/RepositoryInterface.php
+++ src/Repository/RepositoryInterface.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Repository;
+use App\Collection\CollectionInterface;
+
 interface RepositoryInterface
 {
     public function findAll(string $order = '');
     public function findById(int $id);
     public function findBy(string $column, mixed $value, string $order = '');
     public function findOneBy(string $column, mixed $value);
-    public function save(object $entity);
-    public function delete(object $entity);
-    public function closeDB();
+    public function save(object $entity): void;
+    public function delete(object $entity): void;
+    public function closeDB(): void;
 }
