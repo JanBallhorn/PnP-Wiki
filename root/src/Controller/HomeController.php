58c00abfce928ef19e74e161823421d07e21bdf2
Jan Ballhorn <janhannes@outlook.de>
2024-08-22 20:02:20 +0200

    Rechtschreibfehler in home.twig

--- root/src/Controller/HomeController.php
+++ root/src/Controller/HomeController.php
@@ -2,9 +2,19 @@
 
 namespace App\Controller;
 
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
 class HomeController extends Controller
 {
     private string $template = 'home.twig';
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function __construct(){
         $this->render($this->template);
     }
