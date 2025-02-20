5f823591222877000caee4b57904f92bf4d0b823
Jan Ballhorn <janhannes@outlook.de>
2025-02-20 12:18:11 +0100

    Try-Catch in index.php hinzugefügt

--- root/index.php
+++ root/index.php
@@ -9,4 +9,8 @@ require_once __DIR__ . '/src/Controller/RegisterController.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 $router = new Router($uri);
-$router->execRoute();
+try {
+    $router->execRoute();
+} catch (ReflectionException $e) {
+    echo $e->getMessage();
+}
